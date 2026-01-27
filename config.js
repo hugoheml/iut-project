a542574f7658a06eb2dbcaf27a4dc7ff7ec8d255
Hugo H. <hugoheml@users.noreply.github.com>
2026-01-27 17:00:09 +0100

    feat(login): add jwt strategy

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	JWT_TOKEN_KEY: 'RANDOM_SECRET_KEY_TO_CHANGE' // Je sais que ce n'est pas la meilleur manière de faire, mais comme on a pas vu les variables d'environnement, j'ai préféré le faire comme ça.
+};
